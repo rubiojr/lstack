700d186fa10437942fe70bad8e4b69a98f865d36
Sergio Rubio <rubiojr@frameos.org>
2014-10-26 23:28:18 +0100

    destroy command fixes
    Handle the case when the container is stopped gracefuly

--- lib/commands/destroy.sh
+++ lib/commands/destroy.sh
@@ -10,8 +10,16 @@ loopdev=loop6
 check_distro
 needs_root
 
-# If the container was not fully provisioned vgrename may not be there
+# FIXME: not sure if this is actually required
+# If the container is stopped, we need to boot it to clean the
+# volume group and the loop device.
+if lxc-info -n lstack | grep STOPPED >/dev/null; then
+  warn "Container stopped. Booting it to clean it up."
+  lxc-start -n lstack -d
+  wait_for_container_ip
+fi
 
+# If the container was not fully provisioned vgrename may not be there
 if cexe "lstack" "which vgremove" > /dev/null; then
   debug "Removing the LVM volume"
   # try to remove the volume group only if it's there

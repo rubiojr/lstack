70cb6230cb384637b4203756d156fabd2306a5c2
Sergio Rubio <rubiojr@frameos.org>
2014-10-26 13:56:27 +0100

    Hardlink image instead of bind mount dir

--- lib/commands/importimg.sh
+++ lib/commands/importimg.sh
@@ -5,10 +5,9 @@ set -e
 BASE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 CMD_PATH="${BASH_SOURCE[0]}"
 source $BASE_PATH/lstack.sh
+source $BASE_PATH/install/creds.sh
 image=$1
 cname=lstack
-
-source $BASE_PATH/commands/bootstrap.sh
 
 if ! [ -f "$image" ]; then
   echo "Invalid image file $image."
@@ -20,7 +19,26 @@ file "$image" | grep "QCOW Image" || {
   exit 1
 }
 
+if [ `whoami` != "root" ]; then
+  debug "Need to run as root, trying sudo"
+  exec sudo bash $CMD_PATH $@
+fi
+
 image_name=$(basename "$image")
-mount -o bind $(dirname "$image") /var/lib/lxc/$cname/rootfs/mnt/
-cexe "$cname" "glance image-create --name $image_name --is-public true --container-format bare --disk-format qcow2 --file /mnt/$image_name"
+
+# hardlink the image to the container
+ln "$image" "/var/lib/lxc/$cname/rootfs/tmp/$image_name"
+info "Importing the image into Glance..."
+cexe "$cname" "glance --os-username=$OS_USERNAME \
+                      --os-password=$OS_PASSWORD \
+                      --os-tenant-name $OS_TENANT_NAME \
+                      --os-auth-url $OS_AUTH_URL \
+                      image-create --name $image_name \
+                      --is-public true \
+                      --container-format bare \
+                      --disk-format qcow2 \
+                      --file /tmp/$image_name" || {
+  error "Error importing the image"
+  rm -f "/var/lib/lxc/$cname/rootfs/tmp/$image_name"
+}
 

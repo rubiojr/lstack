4df10af27cbe97a425693a86b19664b238cf8636
Sergio Rubio <rubiojr@frameos.org>
2014-10-26 14:11:36 +0100

    Check the image checksum after the import

--- lib/commands/importimg.sh
+++ lib/commands/importimg.sh
@@ -14,7 +14,7 @@ if ! [ -f "$image" ]; then
   exit 1
 fi
 
-file "$image" | grep "QCOW Image" || {
+file "$image" | grep "QCOW Image" > /dev/null 2>&1 || {
   echo "Invalid image. Only QCOW images supported for now."
   exit 1
 }
@@ -29,7 +29,7 @@ image_name=$(basename "$image")
 # hardlink the image to the container
 ln "$image" "/var/lib/lxc/$cname/rootfs/tmp/$image_name"
 info "Importing the image into Glance..."
-cexe "$cname" "glance --os-username=$OS_USERNAME \
+img_id=$(cexe "$cname" "glance --os-username=$OS_USERNAME \
                       --os-password=$OS_PASSWORD \
                       --os-tenant-name $OS_TENANT_NAME \
                       --os-auth-url $OS_AUTH_URL \
@@ -37,8 +37,16 @@ cexe "$cname" "glance --os-username=$OS_USERNAME \
                       --is-public true \
                       --container-format bare \
                       --disk-format qcow2 \
-                      --file /tmp/$image_name" || {
+                      --file /tmp/$image_name | grep '\sid\s' | awk '{print \$4}'") || {
   error "Error importing the image"
   rm -f "/var/lib/lxc/$cname/rootfs/tmp/$image_name"
 }
+
+md5=$(md5sum "$image" | awk '{print $1}')
+glance_md5=$(cexe "$cname" "glance image-show $image_name | grep checksum | awk '{print \$4}'")
+if [ "$md5" = "$glance_md5" ]; then
+  info "Image imported"
+else
+  error "The MD5 of the imported image does not match the source MD5"
+fi
 

29b31a17267648a3e07df9b68573d52f80826b76
Sergio Rubio <rubiojr@frameos.org>
2014-10-28 19:37:56 +0100

    fix volid

--- lib/commands/deploy.sh
+++ lib/commands/deploy.sh
@@ -36,14 +36,14 @@ main(){
                                         --flavor $deploy_flavor \
                                         "$deploy_name" > /dev/null
   else
-    __volid=$(cinder_create "10")
+    __volid=$(cinder_create "$deploy_volume")
     if [ -z "$__volid" ]; then
       error "Could not create the Cinder volume!"
       exit 1
     fi
     source $BASE_PATH/commands/nova.sh boot \
                                         --image "$gid" \
-                                        --block-device source=volume,id=__volid,dest=volume,shutdown=preserve \
+                                        --block-device source=volume,id=$__volid,dest=volume,shutdown=preserve \
                                         --flavor $deploy_flavor \
                                         "$deploy_name" > /dev/null
   fi

447085e913bc4389b6da2ffbecbf66f0fe4ea5fd
Sergio Rubio <rubiojr@frameos.org>
2014-11-01 20:08:21 +0100

    Simplify the ip command

--- lib/commands/ip.sh
+++ lib/commands/ip.sh
@@ -7,24 +7,4 @@ source $BASE_PATH/lstack.sh
 
 needs_root
 
-ips=$(lxc-info -i -n $LSTACK_NAME | cut -d' ' -f2- | tac | xargs ) || true
-if [ -z "$ips" ]; then
-  error "Container IP not found"
-  exit 1
-fi
-
-# The container will have multiple IPs because nova-network so we wanna
-# try them all till we find the one that is reachable from the host.
-ssh_port=$(config_get "lstack.ssh_port" "22")
-for ip in $ips; do
-  ssh -q -o StrictHostKeyChecking=no \
-      -p "$ssh_port" \
-      -o ConnectTimeout=2 \
-      -o UserKnownHostsFile=/dev/null \
-      -l root \
-      -i ~/.config/lstack/sshkey \
-      $ip true || continue
-  # We've found the IP
-  echo $ip
-  break
-done
+echo $(sshable_ip $LSTACK_NAME)

831dd9f92e59c459890c441e6adb302f692284dd
Sergio Rubio <rubiojr@frameos.org>
2014-11-01 23:14:50 +0100

    Update the cache first

--- script/vagrant_provision.sh
+++ script/vagrant_provision.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e
+
+sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:rubiojr/lstack
 sudo add-apt-repository -y ppa:ubuntu-lxc/stable
 sudo apt-get update
 sudo apt-get install -y lstack
 
-sudo lstack bootstrap
+sudo lstack --nonyancat bootstrap

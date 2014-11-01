152fae94b78cd2dfc6c5fbb11ee7704985884ad5
Sergio Rubio <rubiojr@frameos.org>
2014-11-01 23:12:20 +0100

    Add ubuntu-lxc/stable ppa

--- script/vagrant_provision.sh
+++ script/vagrant_provision.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:rubiojr/lstack
+sudo add-apt-repository -y ppa:ubuntu-lxc/stable
 sudo apt-get update
 sudo apt-get install -y lstack
 

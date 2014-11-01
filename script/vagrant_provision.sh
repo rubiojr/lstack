3783d2f28558ca4ce1441d627330c394f10664e3
Sergio Rubio <rubiojr@frameos.org>
2014-11-01 23:16:09 +0100

    Use the right package for precise

--- script/vagrant_provision.sh
+++ script/vagrant_provision.sh
@@ -2,7 +2,7 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install -y software-properties-common
+sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:rubiojr/lstack
 sudo add-apt-repository -y ppa:ubuntu-lxc/stable
 sudo apt-get update

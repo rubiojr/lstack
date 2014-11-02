740821823347d19a378cde8b28b14b30566e4156
Sergio Rubio <rubiojr@frameos.org>
2014-11-02 21:57:18 +0100

    Use test helper

--- test/bootstrap.bat
+++ test/bootstrap.bat
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-lstack(){
-  sudo ./lstack --nocolor --quiet $@
-}
+load test_helper
 
 @test "bootstrap" {
   lstack bootstrap
 }
 
-@test "bootstrap fails" {
+@test "bootstrapping when already running fails" {
   lstack bootstrap 2>&1 | grep "Container already running"
 }
 
